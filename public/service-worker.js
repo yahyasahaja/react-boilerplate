72b2073539ef3a10f7fd5ce1a8fd05aba3f635b5
Yahya Sahaja <yahya.sahaja2@gmail.com>
2018-01-04 16:39:57 +0700

    apalah

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,61 +1,87 @@
-// Set this to true for production
-var doCache = true;
+// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// Name our cache
-var CACHE_NAME = 'react-pwa-boilerplate-v2';
+var dataCacheName = 'abc';
+var cacheName = 'def';
+var filesToCache = [
+  '/',
+  '/index.html',
+];
 
-// Delete old caches that are not our current one!
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys()
-      .then(keyList =>
-        Promise.all(keyList.map(key => {
-          if (!cacheWhitelist.includes(key)) {
-            console.log('Deleting cache: ' + key)
-            return caches.delete(key);
-          }
-        }))
-      )
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
   );
 });
 
-// The first time the user starts up the PWA, 'install' is triggered.
-self.addEventListener('install', function (event) {
-  if (doCache) {
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function (cache) {
-          // Get the assets manifest so we can see what our js file is named
-          // This is because webpack hashes it
-          const urlsToCache = [
-            '/',
-            '/index.html',
-            '/manifest.json',
-            '/favicon.ico',
-            '/app.bundle.js',
-            '/css/style.css',
-            '/font/MuseoSans-100.otf',
-            '/font/MuseoSans-300.otf',
-            '/font/MuseoSans-500.otf',
-            '/font/MuseoSans-700.otf',
-            '/font/MuseoSans-900.otf',
-          ]
-
-          cache.addAll(urlsToCache)
-          console.log('cached');
-        })
-    );
-  }
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  /*
+   * Fixes a corner case in which the app wasn't returning the latest data.
+   * You can reproduce the corner case by commenting out the line below and
+   * then doing the following steps: 1) load app for first time so that the
+   * initial New York City data is shown 2) press the refresh button on the
+   * app 3) go offline 4) reload the app. You expect to see the newer NYC
+   * data, but you actually see the initial data. This happens because the
+   * service worker is not yet activated. The code below essentially lets
+   * you activate the service worker faster.
+   */
+  return self.clients.claim();
 });
 
-// When the webpage goes to fetch files, we intercept that request and serve up the matching files
-// if we have them
-self.addEventListener('fetch', function (event) {
-  if (doCache) {
-    event.respondWith(
-      caches.match(event.request).then(function (response) {
-        return response || fetch(event.request);
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
       })
     );
   }
